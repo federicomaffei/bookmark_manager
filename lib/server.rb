5abc5f5ff6b2105d480b7681c6d9cd057a8d3c70
federicomaffei <federico.maffei@gmail.com>
2014-06-16 19:36:22 +0100

    first test over link listing

--- lib/server.rb
+++ lib/server.rb
@@ -1,3 +1,4 @@
+require 'sinatra'
 require 'data_mapper'
 env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
@@ -10,3 +11,8 @@ DataMapper.finalize
 
 # However, the database tables don't exist yet. Let's tell datamapper to create them
 DataMapper.auto_upgrade!
+
+get '/' do
+  @links = Link.all
+  erb :index
+end
