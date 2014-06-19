dd4d8d27f9c7534d3854721d5cf8b04348100f1d
federicomaffei <federico.maffei@gmail.com>
2014-06-19 21:06:51 +0100

    password retrieval feature added - one test fixed

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -59,22 +59,31 @@ feature "User signs up" do
   # to keep the example simple.
 
   scenario "when being logged out" do    
-    lambda { sign_up }.should change(User, :count).by(1)    
+    expect(lambda { sign_up }).to change(User, :count).by(1)    
     expect(page).to have_content("Welcome, alice@example.com")
     expect(User.first.email).to eq("alice@example.com")        
   end
   
   scenario "with a password that doesn't match" do
-    lambda { sign_up('a@a.com', 'pass', 'wrong') }.should change(User, :count).by(0)
+    expect(lambda { sign_up('a@a.com', 'pass', 'wrong') }).to change(User, :count).by(0)
     expect(current_path).to eq('/users')   
     expect(page).to have_content("Sorry, your passwords don't match")
   end
 
   scenario "with an email that is already registered" do    
-    lambda { sign_up }.should change(User, :count).by(1)
-    lambda { sign_up }.should change(User, :count).by(0)
+    expect(lambda { sign_up }).to change(User, :count).by(1)
+    expect(lambda { sign_up }).to change(User, :count).by(0)
     expect(page).to have_content("This email is already taken")
   end
 
 end
 
+
+feature "User forgets the password" do
+
+  scenario 'and visits a page that allows the recovery' do
+    visit('/users/reset_password')
+    expect(page).to have_content('Please enter your email to recover the password.')
+  end
+
+end
